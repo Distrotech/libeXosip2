4702a015393bfaca3464c3f0245592c7bd8fd988
Aymeric Moizard <amoizard@gmail.com>
2014-01-27 16:38:41 +0100

    improve allocation for UDP (fix compilation)

--- src/eXosip2.h
+++ src/eXosip2.h
@@ -461,6 +461,7 @@ extern "C" {
     char dtls_firewall_port[10];
 
     CbSipCallback cbsipCallback;
+    int masquerade_via;
   };
 
   int _eXosip_guess_ip_for_via (struct eXosip_t *excontext, int family, char *address, int size);
