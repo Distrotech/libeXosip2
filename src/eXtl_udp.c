cc23f69e4e0031d91689782811bc70070e4a378e
Aymeric Moizard <amoizard@gmail.com>
2014-06-26 14:37:58 +0200

    add SO_REUSEADDR to UDP socket

--- src/eXtl_udp.c
+++ src/eXtl_udp.c
@@ -192,6 +192,12 @@ udp_tl_open (struct eXosip_t *excontext)
 #endif /* IPV6_V6ONLY */
     }
 
+    {
+      int valopt = 1;
+
+      setsockopt (sock, SOL_SOCKET, SO_REUSEADDR, (void *) &valopt, sizeof (valopt));
+    }
+
 #if SO_NOSIGPIPE
     {
       int val;
